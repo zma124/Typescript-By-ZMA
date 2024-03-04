471a0630aadc8e570d77200bd8ede6a8b6d4b253
zma124 <151908176+zma124@users.noreply.github.com>
2024-03-04 22:08:10 +0500

    CLASS PROJECT
    This is a class project project that was given by sir on tuesday 27-02-2024.

---
+++ index.js
@@ -0,0 +1,12 @@
+// Example 1 string
+var personName = "Malik Zubair Awan";
+console.log("Asalam O Alaikum,", personName, "!");
+//Example 2 number
+var personAge = 17;
+console.log(personAge);
+//Example 3 boolean
+var isstudent = true;
+console.log(isstudent);
+//Example 4 Null
+var use = null;
+console.log(null);
---
+++ index.ts
@@ -0,0 +1,16 @@
+
+// Example 1 string
+let personName: string = "Malik Zubair Awan"
+console.log("Asalam O Alaikum,", personName, "!")
+
+//Example 2 number
+let personAge: number = 17;
+console.log(personAge)
+
+//Example 3 boolean
+let isstudent: boolean = true
+console.log(isstudent)
+
+//Example 4 Null
+let use: null = null; 
+console.log(null)
